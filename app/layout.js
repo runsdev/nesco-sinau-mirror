90b1aa56a1d6db45ee57e9b0e408dfc6e2d8ed1b
mie-intel <paryapradhanika@gmail.com>
2024-12-17 08:29:05 +0700

    fix[Beranda]: memperbaiki urutan

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,7 @@ export const metadata = {
   },
 };
 
+// eslint-disable-next-line react/prop-types
 export default function RootLayout({ children }) {
   return (
     <html lang="id-ID">
--- app/page.js
+++ app/page.js
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <>
       <Nasta />
+      <Tasya />
       <Josh />
       <Stella />
-      <Tasya />
     </>
   );
 }
