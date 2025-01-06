f2edc33f6fcf9c341aedf80306a7d98aa6f00fc1
Monica@Ideapad-tasya <monicasyatasya@gmail.com>
2025-01-06 08:53:03 +0700

    tentang porsenigama

--- src/modules/Tasya.js
+++ src/modules/Tasya.js
@@ -3,10 +3,27 @@
   Semangat Tas!
 */
 
+import Image from "next/image";
+
 export default function Tasya() {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-zinc-600 text-[30px]">
-      Tasya
-    </div>
+    <>
+      <div className="flex min-h-screen w-full flex-col items-center bg-white">
+        <div className="mt-[55px] text-[35px]">Tentang</div>
+        <div className="text-[35px]">Porsenigama 2024</div>
+
+        <div className="mt-[24px] flex w-full justify-start pl-[200px]">
+          <div className="box-border h-[150px] w-[250px] rounded-[15px] border-[3px] border-black p-4" />
+        </div>
+
+        <div className="mt-[24px] flex w-full justify-start pl-[200px]">
+          <Image src="/images/tenis.png" width={500} height={500} alt="Picture of the author" />
+        </div>
+
+        <div className="mt-[100px] flex w-full justify-start pl-[200px]">
+          <div className="box-border h-[150px] w-[300px] rounded-[15px]" />
+        </div>
+      </div>
+    </>
   );
 }
